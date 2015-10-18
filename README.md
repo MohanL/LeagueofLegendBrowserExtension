e672b0a9470a38eed632abaa79ebdb4fb985314f
Mohan Liu <mohan.liu.personal@gmail.com>
2015-10-18 13:23:05 -0400

    partially working

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 Chrome/Firefox Extensions
 
 This extension automatically detects summoner zeroelov's game status
+Goal for now: 1. popup to show if zeroelov is in a game. 
+              2. check out past games in 10 hours and return prediction. 
+              3. differentiate time (diurnal or nocturnal)
+
+Implementation details: 
+			  1. Incorporating League of Legend API into JS. Using following API:
+			  	 a. summoner-v1.4  
+			  	    na.api.pvp.net/api/lol/{region}/v1.4/summoner/by-name/{summonerNames}?api_key=
+			  	    need to retrive summonerID
+			  	 b. current-game-v1.0 
+			  	    /observer-mode/rest/consumer/getSpectatorGameInfo/{platformId}/{summonerId}api_key=
+			  	   	check game time. 
+			  	 c. game-v1.3
+			  	 	/api/lol/{region}/v1.3/game/by-summoner/{summonerId}/recent
+			  	    retrieve recent 10 games information:
+			  	    analyze the time of the games
+Progress: 
+			  1. retrieve SummonerID and SummerName.
--- test/check.js
+++ test/check.js
@@ -1,10 +1,80 @@
-/* first step 
- * get the alarm to work
- * set up a time and trigger something
- */
  
  function renderStatus(statusText) {
   document.getElementById('status').textContent = "Hi how are you?";
+  summonerIdLookUp();
+  //summonerCurrentGame();
 }
+
+function summonerIdLookUp() {
+   var SUMMONER_NAME = "zeroelov";
+   var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
+   var SUMMONER_ID= "";
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+   if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var myArr = JSON.parse(xhttp.responseText);
+      var SUMMONER_ID = myArr["zeroelov"].id;
+      document.getElementById("status").innerHTML = SUMMONER_ID;
+      summonerCurrentGame();
+    }
+   }
+   xhttp.open("GET", "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+SUMMONER_NAME+"?api_key="+API_KEY, true);
+   xhttp.send();
+
+}
+function summonerCurrentGame(){
+   var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+   //if (xhttp.readyState == 4 && xhttp.status == 200) {
+      //var myArr = JSON.parse(xhttp.responseText);
+   //   document.getElementById("currentgame").innerHTML = "in a game";
+      //document.getElementById("status").innerHTML = xhttp.responseText;
+   // }
+   // else if(xhttp.readyState == 4 && xhttp.status == 0)
+   // {
+   //   document.getElementById("currentgame").innerHTML = "not in a game";
+   // }
+   // else
+   // {
+      if(xhttp.responseText!="")
+        document.getElementById("currentgame").innerHTML = xhttp.responseText;
+      else
+        document.getElementById("currentgame").innerHTML = "Not in a game";
+
+   // }
+   }
+   xhttp.open("GET", 
+   "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/38996502?api_key=6ee2214a-80ef-4554-89f3-80aa72c22f29",
+   true);
+   //xhttp.open("GET", "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/38996502?api_key="+API_KEY, true);
+   //"https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/"+SUMMONER_ID+"?api_key="+API_KEY, true);
+   xhttp.send();
+}
+
+//funtion summonerPastGames()
+//{
+//  var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//   if (xhttp.readyState == 4 && xhttp.status == 200) {
+//      //var myArr = JSON.parse(xhttp.responseText);
+//      document.getElementById("currentgame").innerHTML = "In a Game";
+//      //document.getElementById("status").innerHTML = xhttp.responseText;
+//    }
+//    else if(xhttp.readyState == 4 && xhttp.status == 0)
+//    {
+//      document.getElementById("currentgame").innerHTML = "Not in a Game";
+//    }
+//    else
+//    {
+//      document.getElementById("currentgame").innerHTML = "usnexpected";
+//    }
+//   }
+//   xhttp.open("GET", "https://na.api.pvp.net/api/lol/{region}/v1.3/game/by-summoner//38996502?api_key=" +API_KEY, true);
+//   //"https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/"+SUMMONER_ID+"?api_key="+API_KEY, true);
+//   xhttp.send();
+//}
+
 
 document.addEventListener('DOMContentLoaded', function(){ renderStatus("Hello World!"); });
