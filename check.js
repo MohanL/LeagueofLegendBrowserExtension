4973ce9148d5b975b15e07d0e7cd614eca991893
Mohan Liu <mohan.liu.personal@gmail.com>
2015-11-06 00:56:48 -0500

    too many changes, it doesn't work anymore

--- check.js
+++ check.js
@@ -2,17 +2,8 @@
    var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
-   if (xhttp.readyState == 4 && xhttp.status == 200) {
-      //var myArr = JSON.parse(xhttp.responseText);
-      //document.getElementById("currentgame").innerHTML = "In a Game";
-      document.getElementById("pastgames").innerHTML = "success";
-    }
-    else if(xhttp.readyState == 4 && xhttp.status == 0)
-    {
-      document.getElementById("pastgames").innerHTML = xhttp.responseText;
-    }
-    else{
-      document.getElementById("pastgames").innerHTML = "Error2";
+   if (xhttp.readyState == 4) {
+      document.getElementById("pastgames").innerHTML = "asdf";
     }
    }
    xhttp.open("GET", "https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/38996502?api_key=" +API_KEY, true);
@@ -25,11 +16,11 @@
    var SUMMONER_ID= "";
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
-   if (xhttp.readyState == 4 && xhttp.status == 200) {
+   if (xhttp.readyState == 4) {
       var myArr = JSON.parse(xhttp.responseText);
       var SUMMONER_ID = myArr["zeroelov"].id;
-      document.getElementById("status").innerHTML = SUMMONER_ID;
-      //summonerPastGames();
+       document.getElementById("user").innerHTML = SUMMONER_ID;
+      summonerCurrentGame();
     }
    }
    xhttp.open("GET", "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+SUMMONER_NAME+"?api_key="+API_KEY, true);
@@ -37,41 +28,20 @@
 }
 
  function renderStatus(statusText) {
-  //document.getElementById('status').textContent = "Hi how are you?";
-  summonerIdLookUp();
-  //summonerPastGames():
+   summonerIdLookUp();
 }
 
 
-
-
-
-
-
-//function summonerCurrentGame(){
-//   var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//   if (xhttp.readyState == 4 && xhttp.status == 200) {
-//    //var myArr = JSON.parse(xhttp.responseText);
-//      document.getElementById("currentgame").innerHTML = "in a game";
-//    //document.getElementById("status").innerHTML = xhttp.responseText;
-//    }
-//    else if(xhttp.readyState == 4 && xhttp.status == 404)
-//    {
-//      document.getElementById("currentgame").innerHTML = "not in a game";
-//    }
-//    else
-//    {
-//      document.getElementById("currentgame").innerHTML = "current wrong";
-//
-//    }
-//   }
-//   xhttp.open("GET", 
-//   "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/38996502?api_key=6ee2214a-80ef-4554-89f3-80aa72c22f29",
-//   true);
-//   xhttp.send();
-//}
-
-
-document.addEventListener('DOMContentLoaded', function(){ renderStatus("Hello World!"); });
+function summonerCurrentGame(){
+   var API_KEY = "6ee2214a-80ef-4554-89f3-80aa72c22f29";
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+   if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("currentgame").innerHTML = "asdf";
+    }
+   }
+   xhttp.open("GET", 
+   "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/38996502?api_key=6ee2214a-80ef-4554-89f3-80aa72c22f29",
+   true);
+   xhttp.send();
+  }
